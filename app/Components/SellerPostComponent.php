0674817e3c0e1f64ad258f4e093f8ccb0a8536b9
priya9634 <priya@wyzmindz.com>
2016-08-18 12:39:36 +0530

    Add files via upload

--- app/Components/SellerPostComponent.php
+++ app/Components/SellerPostComponent.php
@@ -9,11 +9,44 @@ use App\Models\Seller;
 
 class SellerPostComponent{
     
-    public static function SellerComponent($data){
-        Seller::SellerPostInsertion($data);
+    public static function SellerComponent($data,$nextdata){
+        echo"copo";print_r($nextdata);
+       Seller::SellerPostInsertion($data,$nextdata);
     }
+    
+    
     public static function GetData(){
-        $data = Seller::SellerPost();
+        $data =Seller::SellerPost();
         return $data;
     }
+    
+    
+      public static function PostMasterComponent($data){
+        Seller::PostMasterInsertion($data);
+    }
+   
+	public static function PostMData($id){
+        $data =Seller::PostMGData($id);
+        return $data;
+    }
+    
+    
+     public static function SellerPostMasterTo($user_id){
+        $data=Seller::SellerPostMasterTo($user_id);
+         return $data;
+        }
+    
+    public static function SellerPostMasterFrom($user_id){
+        $data=Seller::SellerPostMasterFrom($user_id);
+         return $data;
+        }
+    
+    public static function SellerPostMasterAll($user_id){
+        $data=Seller::SellerPostMasterAll($user_id);
+         return $data;
+        }
+    
+
+
+    
 }
