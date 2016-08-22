6b07f1f1fd3de8a7ace4cc8ae93754d1dea4822a
nikhil9634 <knikhil@nagarjunagroup.com>
2016-08-22 16:51:04 +0530

    Sprint1
    Policies

--- resources/views/cancellationPolicy.blade.php
+++ resources/views/cancellationPolicy.blade.php
@@ -1,5 +1,89 @@
-@extends('includes.registrationheader')
-@section('content')
+
+<title>Logi stiks</title>
+<link rel="stylesheet" href="/css/normalize.min.css">
+<link rel="stylesheet" href="/css/styles.css">
+<script src="/js/jquery-3.1.0.min.js"></script>
+<link rel="stylesheet" href="/css/text-filed-1.css">
+<script src="/js/text-filed-1.js"></script>
+<link rel="stylesheet" href="/css/jquery-ui.css">
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/datepicker-shortcode.js"></script>
+<link rel="stylesheet" href="/css/accordian.css">
+<script src="/js/accordian-shortcode.js"></script>
+<!-- <script src="/js/text-filed-1-shartcode.js"></script>  -->
+<script src="/js/main.js"></script>
+<script src="/js/addroute.js"></script> 
+<script src="/js/customizeRoute.js"></script> 
+<script src="/js/jquery.validate.min.js"></script>
+<script src="/js/additionalMethods.js"></script>
+
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+
+<body>
+<header>
+ <div class="header-wrapper">
+    <div class="tbl-1">
+      <div class="row-1">
+        <div class="cell-1"><img src="/images/logo.png" alt="Logo"></div>
+        <div class="cell-1">
+          <div class="header-nav-1">
+            <ul>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Market Place</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Community</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Dealers</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Tools</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#"><img src="/images/search.png" width="19" height="19" alt="Search"></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="cell-1">
+          <div class="header-nav-2">
+            <ul>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1" style="position:relative;">
+                  <div style="position:absolute; z-index:999; background:#ff0000; width:20px; height:20px; left:15px; top:-10px; border-radius:100px; line-height:20px; font-size:14px; color:#fff; text-align:center;">5</div>
+                  <img src="/images/messages-header.png" alt="Message"></div>
+                Messages</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1"><img src="/images/posts-header.png" alt="Posts"></div>
+                Posts</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1"><img src="/images/orders-header.png" alt="Orders"></div>
+                Orders</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1"><img src="/images/network-header.png" alt="Network"></div>
+                Network</a></li>
+             
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+   <div class="menu">
+    <div class="wrapper-1">
+        <div class="tbl-1">
+            <div class="row-1">
+                <div class="cell-1">
+                    <div class="menu-nav-1">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Products</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="cell-1">
+                    <div class="menu-nav-2">
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</header>
 
  <div class="main-container">
 	
@@ -15,20 +99,24 @@
                 <h3> Cancellation Policy</h3>
 
     
-<p>Cancellation And Refund Policy</p>
-<p>•	Order cancellation allowed only for buyers</p>
-<p>•	Only Indents (till truck placements) are allowed to be cancelled. In-case after truck allotment and before consignment pickup if the order has to be cancelled, it has to be done by buyer with vendor & there will be no refund from Logistiks.</p>
-<p>Subject to Truck Not Allotted by Seller </p>
-<p>1.	If the cancellation happens 1 day before despatch date, then the refund amount is 50% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
-<p>2.	If the cancellation happens 2 days before despatch date, then the refund amount is 75% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
-<p>3.	If the cancellation happens  3 days before despatch date, then the refund amount is 80% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
-<p>4.	If the cancellation happens  before 4 days and above from despatch date, then the refund amount is 90% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
+<u><b>Cancellation and Refund Policy</b></u>
+<p>•   Order cancellation <u><b>allowed only for buyers</u></b></p>
+<p>•   Only Indents (till truck placements) are allowed to be cancelled. In-case after truck allotment and before consignment pickup if the order has to be cancelled, it has to be done by buyer with vendor & there will be no refund from Logistiks.</p>
+<u><b>Subject to Truck Not Allotted by Seller </b></u>
+<p>1.  If the cancellation happens 1 day before despatch date, then the refund amount is 50% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
+<p>2.  If the cancellation happens 2 days before despatch date, then the refund amount is 75% of the freight value. Service Tax collected will also be refunded subject to statutory norms,</p>
+<p>3.  If the cancellation happens  3 days before despatch date, then the refund amount is 80% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
+<p>4.  If the cancellation happens  before 4 days and above from despatch date, then the refund amount is 90% of the freight value. Service Tax collected will also be refunded subject to statutory norms</p>
+
+<u><b>System Process flow:</b></u>
+
 <p>System Process flow:</p>
-<p>Buyer  Search Input  Search List  Select a vendor and Book Now  GSA  Add to Cart  Check out  Payment  Order Creation  Order Indent </p>
-<p>Buyer  Search Input  Post & Get quote  Offers (Quotes)  Select a vendor and Book Now GSA  Add to Cart  Check out  Payment  Order Creation  Order Indent</p>
+<p>Buyer -> Search Input -> Search List -> Select a vendor and Book Now -> GSA -> Add to Cart -> Check out -> Payment -> Order Creation -> Order Indent </p>
+<p>Buyer -> Search Input -> Post & Get quote -> Offers (Quotes) -> Select a vendor and Book Now-> GSA -> Add to Cart -> Check out -> Payment -> Order Creation -> Order Indent</p>
+
 <p>Cancel button to be  provided under details section of each Indent. On click of cancel button the system validates above mentioned checks and business rules.</p>
 <p>System pop-up is shown “Do you really want to cancel order No -----------“ (Yes / No). On clicking No the system will go back to Order Indent Page.</p>
-<p>On Clicking Yes, Pop-up with a text box – Reason For Cancellation ( user input max 100 character - Mandatory ) and cancellation policy, refund policy and disclaimer for logistiks will appear with a button “ Accept” or “Decline”. On clicking “Decline” the system will go back to Order Indent Page.</p>
+<p>On Clicking Yes, Pop-up with a text box – Reason For Cancellation ( user input max 100 character - Mandatory ) and cancellation policy, refund policy and disclaimer for logistiks will appear with a button "<u><b> Accept </b></u>"or “<u><b> Decline </b></u>”. On clicking “Decline” the system will go back to Order Indent Page.</p>
 <p>On clicking “Accept”, another popup with message showing the details of refundable amount with statement stating refund will be credited back to his transacted account within 7 working days. </p>
 <p>System will send alert message to seller, buyer, call centre, IT Admin, respective franchisee and P&L head through SMS and MAIL.</p>
 <p>The status of the order in Indents will show cancelled.</p>
@@ -108,4 +196,3 @@
 <script src="/js/text-filed-1-shartcode.js"></script>
 <script src="/js/main.js"></script>
 
-@stop
--- resources/views/privacypolicy.blade.php
+++ resources/views/privacypolicy.blade.php
@@ -1,6 +1,89 @@
-@extends('includes.registrationheader')
-@section('content')
 
+<title>Logi stiks</title>
+<link rel="stylesheet" href="/css/normalize.min.css">
+<link rel="stylesheet" href="/css/styles.css">
+<script src="/js/jquery-3.1.0.min.js"></script>
+<link rel="stylesheet" href="/css/text-filed-1.css">
+<script src="/js/text-filed-1.js"></script>
+<link rel="stylesheet" href="/css/jquery-ui.css">
+<script src="/js/jquery-ui.js"></script>
+<script src="/js/datepicker-shortcode.js"></script>
+<link rel="stylesheet" href="/css/accordian.css">
+<script src="/js/accordian-shortcode.js"></script>
+<!-- <script src="/js/text-filed-1-shartcode.js"></script>  -->
+<script src="/js/main.js"></script>
+<script src="/js/addroute.js"></script> 
+<script src="/js/customizeRoute.js"></script> 
+<script src="/js/jquery.validate.min.js"></script>
+<script src="/js/additionalMethods.js"></script>
+
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+
+<body>
+<header>
+ <div class="header-wrapper">
+    <div class="tbl-1">
+      <div class="row-1">
+        <div class="cell-1"><img src="/images/logo.png" alt="Logo"></div>
+        <div class="cell-1">
+          <div class="header-nav-1">
+            <ul>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Market Place</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Community</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Dealers</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">Tools</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#"><img src="/images/search.png" width="19" height="19" alt="Search"></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="cell-1">
+          <div class="header-nav-2">
+            <ul>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1" style="position:relative;">
+                  <div style="position:absolute; z-index:999; background:#ff0000; width:20px; height:20px; left:15px; top:-10px; border-radius:100px; line-height:20px; font-size:14px; color:#fff; text-align:center;">5</div>
+                  <img src="/images/messages-header.png" alt="Message"></div>
+                Messages</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1"><img src="/images/posts-header.png" alt="Posts"></div>
+                Posts</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1"><img src="/images/orders-header.png" alt="Orders"></div>
+                Orders</a></li>
+              <li><a href="http://psmprojects.net/logistiks/ver5/buyer/Buyer_Search_4.html#">
+                <div class="header-icon-1"><img src="/images/network-header.png" alt="Network"></div>
+                Network</a></li>
+             
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+   <div class="menu">
+    <div class="wrapper-1">
+        <div class="tbl-1">
+            <div class="row-1">
+                <div class="cell-1">
+                    <div class="menu-nav-1">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Products</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="cell-1">
+                    <div class="menu-nav-2">
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</header>
  <div class="main-container">
 	
 <!-- LeftNav Content Starts Here -->
@@ -164,4 +247,3 @@ Please remember that if you post any of your Personal Information in public area
 <script src="/js/text-filed-1-shartcode.js"></script>
 <script src="/js/main.js"></script>
 
-@stop
